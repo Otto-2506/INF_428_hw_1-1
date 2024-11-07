7a49d752ce15128de126e5dcce12947a5ceee256
Kuka0007 <mc.kuka04@gmail.com>
2024-11-07 22:08:10 +0500

    Update solution.py

--- task 1/Sets and Dictionaries/solution.py
+++ task 1/Sets and Dictionaries/solution.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        newArr = set()
+        for num in set1:
+            if num in set2:
+                newArr.add(num)
+        return list(newArr)
